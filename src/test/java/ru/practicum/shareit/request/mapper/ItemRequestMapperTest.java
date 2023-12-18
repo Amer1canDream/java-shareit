a06d7a9bfac3a02846029c485eb9fc80c3f7c11b
Artem <tillerman12@gmail.com>
2023-12-18 16:35:01 +0300

    15TZ

--- src/test/java/ru/practicum/shareit/request/mapper/ItemRequestMapperTest.java
+++ src/test/java/ru/practicum/shareit/request/mapper/ItemRequestMapperTest.java
@@ -6,12 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static java.time.LocalDateTime.now;
 import static java.util.List.of;
@@ -57,7 +62,19 @@ public class ItemRequestMapperTest {
                 now(),
                 of()
         );
+
+        var userDto = new UserDto(5,"test","test@test.ru");
+        var itemDto = new ItemDto(7, "test", "test", true, 5, null);
+        var itemRequest = new ItemRequest(1, "test", user, now());
+        var itemRequest1 = ItemRequestMapper.mapToItemRequest(itemRequestDto, userDto);
+        List<ItemDto> items = new ArrayList<>();
+        items.add(itemDto);
+        var itemRequestDto1 = ItemRequestMapper.mapToItemRequestDto(itemRequest);
+        var itemRequestDto2 = ItemRequestMapper.mapToItemRequestDto(itemRequest, items);
         assertThat(itemRequestDto.getDescription()).isEqualTo("Red carpet");
+        assertThat(itemRequest.getDescription().equals(itemRequestDto1.getDescription()));
+        assertThat(itemRequestDto2.getDescription().equals(itemRequest.getDescription()));
+        assertThat(itemRequest1.getDescription().equals(itemRequestDto.getDescription()));
 
     }
 
