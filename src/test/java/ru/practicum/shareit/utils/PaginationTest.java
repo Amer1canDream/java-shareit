9ce8b1534e1637c142dcd52c9edffeaa1cf70f89
Artem <tillerman12@gmail.com>
2023-12-18 20:13:50 +0300

    15TZ

---
+++ src/test/java/ru/practicum/shareit/utils/PaginationTest.java
@@ -0,0 +1,16 @@
+package ru.practicum.shareit.utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class PaginationTest {
+
+    @Test
+    void pagination() {
+        PageRequest pageRequest = Pagination.makePageRequest(1, 1, Sort.by("id"));
+        assertThat(pageRequest.first().equals(0));
+    }
+}
